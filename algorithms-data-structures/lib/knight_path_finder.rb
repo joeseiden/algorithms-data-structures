0abe81bf00802197a626b4212b9d2dd4dd09eb94
App Academy Student <student@appacademy.io>
2017-01-27 14:40:18 -0800

    Add valid moves and new move positions

--- algorithms-data-structures/lib/knight_path_finder.rb
+++ algorithms-data-structures/lib/knight_path_finder.rb
@@ -2,8 +2,40 @@ require_relative '00_tree_node'
 
 class KnightPathFinder
 
-  def initialize
+  MOVES = [
+    [-2,1],
+    [-2,-1],
+    [-1,-2],
+    [1,-2],
+    [2,-1],
+    [2,1],
+    [1,2],
+    [-1,2]
+  ]
 
+  def initialize(start_pos)
+    @start_pos = start_pos
+    @visited_positions = [start_pos]
   end
 
+  def new_move_positions(pos)
+    new_pos = KnightPathFinder.valid_moves(pos).reject do |move|
+      @visited_positions.include?(move)
+    end
+
+    @visited_positions += new_pos
+    new_pos
+  end
+
+  def self.valid_moves(pos)
+    row, col = pos
+    potential_moves = []
+
+    MOVES.each do |coord|
+      new_coord = [row + coord.first, col + coord.last]
+      potential_moves << new_coord unless new_coord.any? { |el| el < 0 or el > 7 }
+    end
+
+    potential_moves
+  end
 end
